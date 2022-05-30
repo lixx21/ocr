dae37b246592e9c7000765b4940025bf365cf23c
Felix Pratamasan <91602612+lixx21@users.noreply.github.com>
2022-05-31 02:06:45 +0800

    Update install_gdown.py

--- install_gdown.py
+++ install_gdown.py
@@ -1,5 +1,5 @@
 import gdown
 
 url ="https://drive.google.com/file/d/1wLaE2mVfVsoM5ym9iRCU4VAfuYrKVBlq/view?usp=sharing"
-output = "ocr-model-5.h5"
+output = "bounding_box_segmentation_5.h5"
 gdown.download(url, output, quiet=False, fuzzy=True)
