69c918bfdf26c784c13f6a136b0d1f310a3768ab
Felix Pratamasan <91602612+lixx21@users.noreply.github.com>
2022-05-30 21:37:06 +0800

    Update install_gdown.py

--- install_gdown.py
+++ install_gdown.py
@@ -1,5 +1,5 @@
 import gdown
 
-url ="https://drive.google.com/file/d/1iD0f0hn461fur_8byxSFp2DiX9n8GCCD/view?usp=sharing"
-output = "ocr-model-Felix-5.h5"
+url ="https://drive.google.com/file/d/1wLaE2mVfVsoM5ym9iRCU4VAfuYrKVBlq/view?usp=sharing"
+output = "ocr-model-5.h5"
 gdown.download(url, output, quiet=False, fuzzy=True)
